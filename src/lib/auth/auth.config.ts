75cb246efd2fe43902ff1e391af92916356ed0ad
ramiro <ramirotuk@gmail.com>
2025-07-17 01:12:54 -0300

    fix: too many redirects because of middleware

--- src/lib/auth/auth.config.ts
+++ src/lib/auth/auth.config.ts
@@ -7,7 +7,7 @@ export const authConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      if (isLoggedIn) return Response.redirect(new URL('/', nextUrl));
+      if (isLoggedIn) return true;
       return false;
     },
   },
